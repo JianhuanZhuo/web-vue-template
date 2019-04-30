48e65010984802e86baa2d0206f9baad342dfbcc
卓建欢 <zhuojianhuan@iie.ac.cn>
2019-04-30 19:55:23 +0800

    init - run

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,55 @@
 var path = require('path');
 var webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: ['babel-polyfill', './src/main.js'],
+    // entry: ['babel-polyfill', './src/main.js'],
+    entry: {
+        app: './src/main.js',
+        // login: './src/login.js',
+    },
     output: {
         path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist/',
-        filename: 'build.js'
+        publicPath: '/', // 在页面中访问所需的 URL 前缀地址,也就是所谓的容器路径
+        filename: '[name].[hash].build.js'
     },
     devServer: {
+        // 静态文件监控地址, 用于自动编译
+        // contentBase: path.join(__dirname, 'dist'),
         historyApiFallback: true,
-        overlay: true
+        // 当出现编译器错误或警告时，在浏览器中显示全屏覆盖层。
+        overlay: true,
+        // compress: true, // 是否启用压缩
+        // 默认端口是 8080
+        // port: 9000,
+        // 默认运行时打开浏览器
+        // open: true,
+        // 首页索引文件
+        index: 'index.html',
+        // 启用热替换功能
+        // hot: true,
+        // after、before 回调
+        after: function (app) {
+            // 做些有趣的事
+        }
     },
     resolve: {
         alias: {
             'vue$': 'vue/dist/vue.esm.js'
         }
     },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            template: path.resolve(__dirname, './src/layout/base.html'), // 指定模板文件
+            filename: 'index.html', // 默认为 index.html
+            chunks: ["app"], // 用于指定加载哪个入口
+            hash: true, // 为静态资源生成hash值
+            // minify: true, // 默认是生产模式为 true，否则为 false
+        })
+    ],
     module: {
         rules: [
             {
@@ -70,6 +103,15 @@ module.exports = {
                             'sass-loader?indentedSyntax'
                         ]
                     }
+                }
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 8192,
+                    name:'[name].[ext]',
+                    outputPath:'assets' //the icons will be stored in dist/assets folder
                 }
             },
         ]
